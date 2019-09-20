1a54a3a3dd9b9cedec71b7cc9d536ef3923d5c40
epochwz <epoch.wz@gmail.com>
2019-09-20 22:48:47 +0800

    demo: ftp-upload

--- src/main/java/fun/epoch/mall/controller/TestService.java
+++ src/main/java/fun/epoch/mall/controller/TestService.java
@@ -2,10 +2,24 @@ package fun.epoch.mall.controller;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import fun.epoch.utils.ftp.FTPUploader;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @Service
-public class TestService {
+public class TestService extends FTPUploader {
     public String upload(MultipartFile file) {
-        return "文件由 service 上传成功：" + file.getOriginalFilename();
+        try {
+            File localFile = Files.createTempFile("", "-" + file.getOriginalFilename()).toFile();
+            file.transferTo(localFile);
+            if (upload(localFile)) {
+                return String.format("<img src='http://file.epoch.fun/mall/%s'><br>文件上传成功：%s", localFile.getName(),localFile.getName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return String.format("文件[%s]上传失败", file.getOriginalFilename());
     }
 }
