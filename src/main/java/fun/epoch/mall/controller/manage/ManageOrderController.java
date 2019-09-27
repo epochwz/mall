828f534446938e7ebd630aa803a88b3253a20c58
epochwz <epoch.wz@gmail.com>
2019-09-27 22:07:36 +0800

    controller: interface define -- manage-order

---
+++ src/main/java/fun/epoch/mall/controller/manage/ManageOrderController.java
@@ -0,0 +1,47 @@
+package fun.epoch.mall.controller.manage;
+
+import com.github.pagehelper.PageInfo;
+import fun.epoch.mall.utils.response.ServerResponse;
+import fun.epoch.mall.vo.OrderVo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller
+@RequestMapping("/manage/order")
+public class ManageOrderController {
+    @ResponseBody
+    @RequestMapping(value = "detail.do")
+    public ServerResponse<OrderVo> detail(@RequestParam long orderNo) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "search.do")
+    public ServerResponse<PageInfo<OrderVo>> search(
+            Long orderNo,
+            Integer userId,
+            Integer status,
+            Long startTime,
+            Long endTime,
+            @RequestParam(defaultValue = "1") int pageNum,
+            @RequestParam(defaultValue = "5") int pageSize
+    ) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "ship.do", method = POST)
+    public ServerResponse ship(@RequestParam long orderNo) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "close.do", method = POST)
+    public ServerResponse close(@RequestParam long orderNo) {
+        return null;
+    }
+}
