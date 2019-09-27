5f74e68250ec293ae2d4c97e261184e7f9beb036
epochwz <epoch.wz@gmail.com>
2019-09-27 20:33:59 +0800

    controller: interface define -- manage-product

---
+++ src/main/java/fun/epoch/mall/controller/manage/ManageProductController.java
@@ -0,0 +1,66 @@
+package fun.epoch.mall.controller.manage;
+
+import com.github.pagehelper.PageInfo;
+import fun.epoch.mall.utils.response.ServerResponse;
+import fun.epoch.mall.vo.ProductVo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller
+@RequestMapping("/manage/product")
+public class ManageProductController {
+    @ResponseBody
+    @RequestMapping(value = "detail.do")
+    public ServerResponse<ProductVo> detail(@RequestParam("id") int productId) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "search.do")
+    public ServerResponse<PageInfo<ProductVo>> search(
+            Integer productId,
+            Integer categoryId,
+            String keyword,
+            @RequestParam(defaultValue = "1") int pageNum,
+            @RequestParam(defaultValue = "5") int pageSize
+    ) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "add.do", method = POST)
+    public ServerResponse<Integer> add(ProductVo productVo) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "update.do", method = POST)
+    public ServerResponse<ProductVo> update(ProductVo productVo) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "shelve.do", method = POST)
+    public ServerResponse shelve(int[] ids, @RequestParam int status) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "upload.do", method = POST)
+    public ServerResponse<String> upload(@RequestParam MultipartFile file) {
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "upload_by_simditor.do", method = POST)
+    public Map<String, Object> uploadBySimditor(@RequestParam MultipartFile file) {
+        return null;
+    }
+}
