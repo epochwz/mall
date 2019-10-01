5a2f48d79bcf4140cacedd97fc438e04c05b1ebf
epochwz <epoch.wz@gmail.com>
2019-10-01 19:08:26 +0800

    dao: LangDriver -- WhereSelectiveLangDriver

---
+++ src/main/java/fun/epoch/mall/dao/langdriver/WhereSelectiveLangDriver.java
@@ -0,0 +1,50 @@
+package fun.epoch.mall.dao.langdriver;
+
+import java.lang.reflect.Field;
+
+import static com.google.common.base.CaseFormat.LOWER_CAMEL;
+import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
+
+/**
+ * select * from table_name <where-selective>
+ */
+public class WhereSelectiveLangDriver extends AbsLangDriver {
+    public static final String WHERE_SELECTIVE = "<where-selective>";
+
+    @Override
+    protected String toDynamicSQL(Class<?> parameterType, String script) {
+        String sql = script.replaceAll(tag(), obj2SelectiveWhereTags(parameterType));
+        return sql2Script(sql);
+    }
+
+    @Override
+    protected String tag() {
+        return WHERE_SELECTIVE;
+    }
+
+    private String obj2SelectiveWhereTags(Class<?> parameterType) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Field field : parameterType.getDeclaredFields()) {
+            if (include(field) || !ignore(field)) {
+                String fieldName = field.getName();
+                String columnName = LOWER_CAMEL.to(LOWER_UNDERSCORE, fieldName);
+                String sqlTag;
+                if (field.getType() == String.class) {
+                    String like = "CONCAT('%',#{" + fieldName + "},'%')";
+                    sqlTag = String.format(" <if test=\"%s != null\"> AND %s like %s</if> ", fieldName, columnName, like);
+                } else {
+                    sqlTag = String.format(" <if test=\"%s != null\"> AND %s=#{%s}</if> ", fieldName, columnName, fieldName);
+                }
+                sb.append(sqlTag);
+            }
+        }
+
+        return String.format("<where>%s</where>", sb.toString());
+    }
+
+    @Override
+    protected boolean include(Field field) {
+        return "id".equals(field.getName());
+    }
+}
