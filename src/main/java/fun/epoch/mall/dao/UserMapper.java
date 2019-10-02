db3d8cbce137367d01d30463ea97889179801ce4
epochwz <epoch.wz@gmail.com>
2019-10-02 21:23:30 +0800

    dao: initial UserMapper

--- src/main/java/fun/epoch/mall/dao/UserMapper.java
+++ src/main/java/fun/epoch/mall/dao/UserMapper.java
@@ -1,5 +1,35 @@
 package fun.epoch.mall.dao;
 
+import fun.epoch.mall.dao.langdriver.InsertAllLangDriver;
+import fun.epoch.mall.dao.langdriver.UpdateAllLangDriver;
+import fun.epoch.mall.dao.langdriver.UpdateSelectiveLangDriver;
+import fun.epoch.mall.entity.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
 public interface UserMapper {
     String selectAll = "select id, username, password, email, mobile, question, answer, role, create_time, update_time from user ";
+
+    @Select(selectAll)
+    List<User> selectAll();
+
+    @Select(selectAll + "where id = #{id}")
+    User selectByPrimaryKey(int id);
+
+    @Delete("delete from user where id = #{id}")
+    int deleteByPrimaryKey(int id);
+
+    @Insert("insert into user <all>")
+    @Lang(InsertAllLangDriver.class)
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    int insert(User user);
+
+    @Update("update user <all>")
+    @Lang(UpdateAllLangDriver.class)
+    int updateByPrimaryKey(User user);
+
+    @Update("update user <selective>")
+    @Lang(UpdateSelectiveLangDriver.class)
+    int updateSelectiveByPrimaryKey(User user);
 }
