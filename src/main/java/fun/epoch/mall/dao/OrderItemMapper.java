c2ae3b9a87cf5423cb1ef770dfc048965f40bd4c
epochwz <epoch.wz@gmail.com>
2019-10-02 21:38:05 +0800

    dao: initial OrderItemMapper

--- src/main/java/fun/epoch/mall/dao/OrderItemMapper.java
+++ src/main/java/fun/epoch/mall/dao/OrderItemMapper.java
@@ -1,5 +1,35 @@
 package fun.epoch.mall.dao;
 
+import fun.epoch.mall.dao.langdriver.InsertAllLangDriver;
+import fun.epoch.mall.dao.langdriver.UpdateAllLangDriver;
+import fun.epoch.mall.dao.langdriver.UpdateSelectiveLangDriver;
+import fun.epoch.mall.entity.OrderItem;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
 public interface OrderItemMapper {
     String selectAll = "select id, order_no, product_id, product_name, product_image, quantity, unit_price, total_price, create_time, update_time from order_item ";
+
+    @Select(selectAll)
+    List<OrderItem> selectAll();
+
+    @Select(selectAll + "where id = #{id}")
+    OrderItem selectByPrimaryKey(int id);
+
+    @Delete("delete from order_item where id = #{id}")
+    int deleteByPrimaryKey(int id);
+
+    @Insert("insert into order_item <all>")
+    @Lang(InsertAllLangDriver.class)
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    int insert(OrderItem orderProductItem);
+
+    @Update("update order_item <all>")
+    @Lang(UpdateAllLangDriver.class)
+    int updateByPrimaryKey(OrderItem orderProductItem);
+
+    @Update("update order_item <selective>")
+    @Lang(UpdateSelectiveLangDriver.class)
+    int updateSelectiveByPrimaryKey(OrderItem orderProductItem);
 }
