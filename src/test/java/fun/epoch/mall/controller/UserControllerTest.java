6491668e5306a98b74f52c500e5a759ec4c14323
epochwz <epoch.wz@gmail.com>
2019-10-03 09:47:30 +0800

    unit-test: portal-user -- controller -- initial

---
+++ src/test/java/fun/epoch/mall/controller/UserControllerTest.java
@@ -0,0 +1,12 @@
+package fun.epoch.mall.controller;
+
+import fun.epoch.mall.controller.portal.UserController;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest {
+    @InjectMocks
+    UserController controller;
+}
