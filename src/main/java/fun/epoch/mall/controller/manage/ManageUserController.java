4514b810e2d25914a9d79c011d0bcf2efc789070
epochwz <epoch.wz@gmail.com>
2019-10-04 22:29:44 +0800

    unit-test: manage-user -- controller -- login

--- src/main/java/fun/epoch/mall/controller/manage/ManageUserController.java
+++ src/main/java/fun/epoch/mall/controller/manage/ManageUserController.java
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
+import static fun.epoch.mall.common.Constant.AccountRole.MANAGER;
+import static fun.epoch.mall.common.Constant.AccountType.USERNAME;
+import static fun.epoch.mall.common.Constant.CURRENT_USER;
+import static fun.epoch.mall.controller.common.Checker.checkPassword;
+import static fun.epoch.mall.controller.common.Checker.checkUsername;
+import static fun.epoch.mall.utils.response.ResponseCode.FORBIDDEN;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
@@ -22,7 +28,20 @@ public class ManageUserController {
     @ResponseBody
     @RequestMapping(value = "login.do", method = POST)
     public ServerResponse<User> login(HttpSession session, @RequestParam String username, @RequestParam String password) {
-        return null;
+        if (checkPassword(password) && checkUsername(username)) {
+            ServerResponse<User> loginResult = userService.login(username, password, USERNAME);
+            if (loginResult.isSuccess()) {
+                User currentUser = loginResult.getData();
+                if (currentUser != null && MANAGER == currentUser.getRole()) {
+                    session.setAttribute(CURRENT_USER, currentUser);
+                    return loginResult;
+                }
+                return ServerResponse.error(FORBIDDEN, "无访问权限 (不是管理员账号)");
+            }
+            return loginResult;
+
+        }
+        return ServerResponse.error("参数格式不正确");
     }
 
     @RequestMapping(value = "logout.do")
--- src/test/java/fun/epoch/mall/controller/manage/ManageUserControllerTest.java
+++ src/test/java/fun/epoch/mall/controller/manage/ManageUserControllerTest.java
@@ -1,11 +1,23 @@
 package fun.epoch.mall.controller.manage;
 
+import fun.epoch.mall.entity.User;
 import fun.epoch.mall.service.UserService;
+import fun.epoch.mall.utils.response.ServerResponse;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.web.MockHttpSession;
+
+import static fun.epoch.mall.common.Constant.AccountRole.CONSUMER;
+import static fun.epoch.mall.common.Constant.AccountRole.MANAGER;
+import static fun.epoch.mall.common.Constant.AccountType.USERNAME;
+import static fun.epoch.mall.common.Constant.CURRENT_USER;
+import static fun.epoch.mall.common.enhanced.TestHelper.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ManageUserControllerTest {
@@ -18,4 +30,44 @@ public class ManageUserControllerTest {
     @Mock
     private MockHttpSession session;
 
+    /**
+     * 登录
+     * <p>
+     * 400  非法参数：其中一个参数不合法
+     * 403  权限不足：账号登录成功，但不是管理员账号
+     * 200  登录成功：账号登录成功，并设置 session
+     */
+    @Test
+    public void testLogin_returnError_whenOneOfParamIsInvalid() {
+        testIfCodeEqualsError(errorUsernames, errorUsername -> controller.login(session, errorUsername, password));
+        testIfCodeEqualsError(errorPasswords, errorPassword -> controller.login(session, username, errorPassword));
+    }
+
+    @Test
+    public void testLogin_returnForbidden_whenLoginSuccessButNotManager() {
+        User loginUser = User.builder().username(username).password(password).role(CONSUMER).build();
+        when(service.login(username, password, USERNAME)).thenReturn(ServerResponse.success(loginUser));
+
+        testIfCodeEqualsForbidden(controller.login(session, username, password));
+
+        verify(session, never()).setAttribute(eq(CURRENT_USER), any());
+    }
+
+    @Test
+    public void testLogin_returnSuccess_whenLoginSuccess_andThenSetUserIntoSession() {
+        User loginUser = User.builder().username(username).password(password).role(MANAGER).build();
+        when(service.login(username, password, USERNAME)).thenReturn(ServerResponse.success(loginUser));
+
+        testIfCodeEqualsSuccess(controller.login(session, username, password));
+
+        verify(session).setAttribute(CURRENT_USER, loginUser);
+    }
+
+    // 合法值
+    private static final String username = "epoch";
+    private static final String password = "epoch_password";
+
+    // 错误值
+    private static final String[] errorPasswords = {null, "", " ", "\t", "\n", "short", "ThisIsAErrorPasswordLongerThan32Chars"};
+    private static final String[] errorUsernames = {null, "", " ", "\t", "\n", "ThisIsAErrorUsernameLongerThan20Chars", "error@username"};
 }
