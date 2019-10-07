d7e45c54704679b2daea9c13cc055f493d928319
epochwz <epoch.wz@gmail.com>
2019-10-07 15:44:10 +0800

    union-test: keys -- tables

---
+++ src/test/java/fun/epoch/mall/mvc/common/Keys.java
@@ -0,0 +1,20 @@
+package fun.epoch.mall.mvc.common;
+
+/**
+ * 测试常量
+ */
+public interface Keys {
+    /**
+     * 数据库表
+     */
+    interface Tables {
+        String user = "user";
+        String shipping = "shipping";
+        String category = "category";
+        String product = "product";
+        String cart_item = "cart_item";
+        String order = "`order`";
+        String order_item = "order_item";
+        String payment_info = "payment_info";
+    }
+}
