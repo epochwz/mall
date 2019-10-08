445cc8744c21b703575201d1ca47941c822bcabe
epochwz <epoch.wz@gmail.com>
2019-10-08 11:17:48 +0800

    union-test: portal-user -- register

--- src/test/java/fun/epoch/mall/mvc/UserTest.java
+++ src/test/java/fun/epoch/mall/mvc/UserTest.java
@@ -1,12 +1,19 @@
 package fun.epoch.mall.mvc;
 
+import fun.epoch.mall.entity.User;
 import fun.epoch.mall.mvc.common.CustomMvcTest;
-import fun.epoch.mall.mvc.common.Keys;
 import org.junit.Before;
+import org.junit.Test;
 
 import static fun.epoch.mall.common.Constant.AccountRole.CONSUMER;
+import static fun.epoch.mall.mvc.common.Apis.portal.user.register;
+import static fun.epoch.mall.mvc.common.Keys.ErrorKeys.*;
+import static fun.epoch.mall.mvc.common.Keys.MockSqls.AUTO_INCREMENT;
 import static fun.epoch.mall.mvc.common.Keys.MockSqls.COMMON_SQLS;
-import static fun.epoch.mall.mvc.common.Keys.UserKeys.userId;
+import static fun.epoch.mall.mvc.common.Keys.Tables.user;
+import static fun.epoch.mall.mvc.common.Keys.UserKeys.*;
+import static fun.epoch.mall.utils.response.ResponseCode.CONFLICT;
+import static fun.epoch.mall.utils.response.ResponseCode.SUCCESS;
 
 public class UserTest extends CustomMvcTest {
     @Before
@@ -14,6 +21,30 @@ public class UserTest extends CustomMvcTest {
         this.init()
                 .session(userId, CONSUMER)
                 .database(COMMON_SQLS)
-                .launchTable(Keys.Tables.user);
+                .launchTable(user);
+    }
+
+    /**
+     * 注册
+     * <p>
+     * 200  注册成功，返回新增用户 id
+     * 409  用户名 / 邮箱 / 手机号码 已存在
+     */
+    @Test
+    public void testRegister_200_withUserId() {
+        this.database().reset(AUTO_INCREMENT, user).launch();
+
+        postJson(register, mockNewUser().build(), SUCCESS, userId);
+    }
+
+    @Test
+    public void testRegister_409_whenAccountAlreadyExist() {
+        postJson(register, mockNewUser().username(username).build(), CONFLICT);
+        postJson(register, mockNewUser().email(email).build(), CONFLICT);
+        postJson(register, mockNewUser().mobile(mobile2).build(), CONFLICT);
+    }
+
+    private User.UserBuilder mockNewUser() {
+        return User.builder().username(usernameNotExist).email(emailNotExist).mobile(mobileNotExist).password(passwordNotExist);
     }
 }
--- src/test/java/fun/epoch/mall/mvc/common/Keys.java
+++ src/test/java/fun/epoch/mall/mvc/common/Keys.java
@@ -22,10 +22,30 @@ public interface Keys {
      * SQL 测试文件
      */
     interface MockSqls {
+        int AUTO_INCREMENT = 1000000;
         String COMMON_SQLS = "mock/tables.sql";
+    }
+
+    interface ErrorKeys {
+        String idNotExist = "999999";
+        String usernameNotExist = "xiaoming";
+        String passwordNotExist = "xiaoming_pass";
+        String emailNotExist = "xiaoming@gmail.com";
+        String mobileNotExist = "15699999999";
     }
 
     interface UserKeys {
         String userId = "1000000";
+        String username = "epoch";
+        String email = "epoch@gmail.com";
+        String mobile = "15622223333";
+        String question = "epoch's question";
+        String answer = "epoch's answer";
+        String password = "epoch_pass";
+
+        String userId2 = "1000001";
+        String username2 = "epochwz";
+        String email2 = "epochwz@gmail.com";
+        String mobile2 = "15611112222";
     }
 }
