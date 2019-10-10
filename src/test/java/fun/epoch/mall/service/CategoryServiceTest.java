1a53755095f1019c8ce545b4aefeb76614c4168e
epochwz <epoch.wz@gmail.com>
2019-10-10 12:09:30 +0800

    unit-test: category -- service -- initial

---
+++ src/test/java/fun/epoch/mall/service/CategoryServiceTest.java
@@ -0,0 +1,11 @@
+package fun.epoch.mall.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CategoryServiceTest {
+    @InjectMocks
+    CategoryService service;
+}
