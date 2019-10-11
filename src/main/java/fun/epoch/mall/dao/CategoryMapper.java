b0e41af6c7cb972f221a86f9716555d86e3dcf88
epochwz <epoch.wz@gmail.com>
2019-10-11 09:14:50 +0800

    unit-test: category -- mapper

--- src/main/java/fun/epoch/mall/dao/CategoryMapper.java
+++ src/main/java/fun/epoch/mall/dao/CategoryMapper.java
@@ -1,5 +1,6 @@
 package fun.epoch.mall.dao;
 
+import fun.epoch.mall.dao.langdriver.ForeachLangDriver;
 import fun.epoch.mall.dao.langdriver.InsertAllLangDriver;
 import fun.epoch.mall.dao.langdriver.UpdateAllLangDriver;
 import fun.epoch.mall.dao.langdriver.UpdateSelectiveLangDriver;
@@ -33,13 +34,19 @@ public interface CategoryMapper {
     @Lang(UpdateSelectiveLangDriver.class)
     int updateSelectiveByPrimaryKey(Category category);
 
+    @Select("select count(1) from category where id=#{id}")
     int selectCountByPrimaryKey(int id);
 
-    int selectCountByParentIdAndCategoryName(int parentId, String categoryName);
+    @Select("select count(1) from category where parent_id=#{parentId} and name=#{categoryName}")
+    int selectCountByParentIdAndCategoryName(@Param("parentId") int parentId, @Param("categoryName") String categoryName);
 
-    int selectCountByParentIdAndCategoryNameExceptCurrentId(int parentId, String categoryName, int categoryId);
+    @Select("select count(1) from category where parent_id=#{parentId} and name=#{categoryName} and id!=#{id}")
+    int selectCountByParentIdAndCategoryNameExceptCurrentId(@Param("parentId") int parentId, @Param("categoryName") String categoryName, @Param("id") int id);
 
-    int updateStatusByPrimaryKey(List<Integer> ids, int status);
+    @Update("update category set status=#{status} where id in <list>")
+    @Lang(ForeachLangDriver.class)
+    int updateStatusByPrimaryKey(@Param("list") List<Integer> ids, @Param("status") int status);
 
+    @Select(selectAll + "where parent_id=#{id}")
     List<Category> selectByParentId(int id);
 }
