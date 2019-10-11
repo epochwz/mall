b9e26c21d2c85be002d68b4b95f197a19d8501ae
epochwz <epoch.wz@gmail.com>
2019-10-11 14:21:48 +0800

    union-test: category -- listAll

--- src/test/java/fun/epoch/mall/mvc/CategoryTest.java
+++ src/test/java/fun/epoch/mall/mvc/CategoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import static fun.epoch.mall.common.Constant.AccountRole.MANAGER;
 import static fun.epoch.mall.common.enhanced.TestHelper.assertObjectEquals;
 import static fun.epoch.mall.mvc.common.Apis.manage.category.list;
+import static fun.epoch.mall.mvc.common.Apis.manage.category.list_all;
 import static fun.epoch.mall.mvc.common.Keys.CategoryKeys.categoryId;
 import static fun.epoch.mall.mvc.common.Keys.ErrorKeys.idNotExist;
 import static fun.epoch.mall.mvc.common.Keys.MockSqls.COMMON_SQLS;
@@ -46,6 +47,28 @@ public class CategoryTest extends CustomMvcTest {
     @Test
     public void testList_200_whenCategoryNotExist() {
         perform(SUCCESS, post(list).param("id", idNotExist));
+    }
+
+    /**
+     * 查询商品类别列表 (递归)
+     * <p>
+     * 200  查询成功 (不指定 id, 默认返回全部类别)
+     * 200  查询成功 (指定 id, 返回商品类别及其递归子类别)
+     * 200  查询成功 (当 id 不存在时)
+     */
+    @Test
+    public void testListAll_200_withCategoriesRecursively_whenNoSpecifiedId() {
+        assertEqualsExpectedJson("mock/category/list_all.json", post(list_all));
+    }
+
+    @Test
+    public void testListAll_200_withCategoriesRecursively_whenSpecifiedId() {
+        assertEqualsExpectedJson("mock/category/list_all_id.json", post(list_all).param("id", categoryId));
+    }
+
+    @Test
+    public void testListAll_200_whenCategoryNotExist() {
+        perform(SUCCESS, post(list_all).param("id", idNotExist));
     }
 
     private void assertEqualsExpectedJson(String expectedJson, MockHttpServletRequestBuilder request) {
