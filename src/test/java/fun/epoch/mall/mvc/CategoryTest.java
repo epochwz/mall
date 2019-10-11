03756ee617b396bd636d63bd0a2f2817e05d3b8c
epochwz <epoch.wz@gmail.com>
2019-10-11 12:28:19 +0800

    union-test: category -- setup

--- src/test/java/fun/epoch/mall/mvc/CategoryTest.java
+++ src/test/java/fun/epoch/mall/mvc/CategoryTest.java
@@ -1,6 +1,19 @@
 package fun.epoch.mall.mvc;
 
 import fun.epoch.mall.mvc.common.CustomMvcTest;
+import org.junit.Before;
+
+import static fun.epoch.mall.common.Constant.AccountRole.MANAGER;
+import static fun.epoch.mall.mvc.common.Keys.MockSqls.COMMON_SQLS;
+import static fun.epoch.mall.mvc.common.Keys.Tables.category;
+import static fun.epoch.mall.mvc.common.Keys.UserKeys.userId;
 
 public class CategoryTest extends CustomMvcTest {
+    @Before
+    public void setup() {
+        this.init()
+                .session(userId, MANAGER)
+                .database(COMMON_SQLS)
+                .launchTable(category);
+    }
 }
