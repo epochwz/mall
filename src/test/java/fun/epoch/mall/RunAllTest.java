dd94a8963c0c8c2d3770ba373daae288daabfbd8
epochwz <epoch.wz@gmail.com>
2019-10-14 23:47:40 +0800

    unit-test: run all test

--- src/test/java/fun/epoch/mall/RunAllTest.java
+++ src/test/java/fun/epoch/mall/RunAllTest.java
@@ -1,9 +1,12 @@
 package fun.epoch.mall;
 
 import fun.epoch.mall.controller.manage.ManageCategoryControllerTest;
+import fun.epoch.mall.controller.manage.ManageProductControllerTest;
 import fun.epoch.mall.controller.manage.ManageUserControllerTest;
+import fun.epoch.mall.controller.portal.ProductControllerTest;
 import fun.epoch.mall.controller.portal.UserControllerTest;
 import fun.epoch.mall.service.CategoryServiceTest;
+import fun.epoch.mall.service.ProductServiceTest;
 import fun.epoch.mall.service.UserServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -11,10 +14,13 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         UserControllerTest.class,
-        UserServiceTest.class,
         ManageUserControllerTest.class,
+        UserServiceTest.class,
         ManageCategoryControllerTest.class,
-        CategoryServiceTest.class
+        CategoryServiceTest.class,
+        ProductControllerTest.class,
+        ManageProductControllerTest.class,
+        ProductServiceTest.class
 })
 public class RunAllTest {
 }
