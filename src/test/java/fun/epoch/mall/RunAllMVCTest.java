d8fdf014d200f5b20e9b63e7c5d3bc1c5ac5f2c8
epochwz <epoch.wz@gmail.com>
2019-10-15 22:51:24 +0800

    union-test: run all test

---
+++ src/test/java/fun/epoch/mall/RunAllMVCTest.java
@@ -0,0 +1,16 @@
+package fun.epoch.mall;
+
+import fun.epoch.mall.mvc.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AuthorityInterceptorTest.class,
+        ExceptionResolverTest.class,
+        UserTest.class,
+        CategoryTest.class,
+        ProductTest.class
+})
+public class RunAllMVCTest {
+}
