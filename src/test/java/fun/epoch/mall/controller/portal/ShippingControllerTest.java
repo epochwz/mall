682a921af315b70faf03826a58c8d5198c1edf5e
epochwz <epoch.wz@gmail.com>
2019-10-18 11:04:20 +0800

    unit-test: shipping -- controller -- setup

--- src/test/java/fun/epoch/mall/controller/portal/ShippingControllerTest.java
+++ src/test/java/fun/epoch/mall/controller/portal/ShippingControllerTest.java
@@ -1,11 +1,17 @@
 package fun.epoch.mall.controller.portal;
 
+import fun.epoch.mall.entity.User;
 import fun.epoch.mall.service.ShippingService;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.web.MockHttpSession;
+
+import static fun.epoch.mall.common.Constant.CURRENT_USER;
+import static fun.epoch.mall.mvc.common.Keys.UserKeys.UserId;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ShippingControllerTest {
@@ -17,4 +23,9 @@ public class ShippingControllerTest {
 
     @Mock
     MockHttpSession session;
+
+    @Before
+    public void setup() {
+        when(session.getAttribute(CURRENT_USER)).thenReturn(User.builder().id(UserId).build());
+    }
 }
