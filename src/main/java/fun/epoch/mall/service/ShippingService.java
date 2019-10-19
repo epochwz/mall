fcc19872a1ad096ac2af626b3014509aab34a652
epochwz <epoch.wz@gmail.com>
2019-10-19 10:12:00 +0800

    unit-test: shipping -- service -- add

--- src/main/java/fun/epoch/mall/service/ShippingService.java
+++ src/main/java/fun/epoch/mall/service/ShippingService.java
@@ -1,14 +1,24 @@
 package fun.epoch.mall.service;
 
 import com.github.pagehelper.PageInfo;
+import fun.epoch.mall.dao.ShippingMapper;
 import fun.epoch.mall.entity.Shipping;
 import fun.epoch.mall.utils.response.ServerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static fun.epoch.mall.utils.response.ResponseCode.INTERNAL_SERVER_ERROR;
 
 @Service
 public class ShippingService {
+    @Autowired
+    ShippingMapper shippingMapper;
+
     public ServerResponse<Integer> add(Shipping shipping) {
-        return null;
+        if (shippingMapper.insert(shipping) > 0) {
+            return ServerResponse.success(shipping.getId());
+        }
+        return ServerResponse.error(INTERNAL_SERVER_ERROR);
     }
 
     public ServerResponse delete(int userId, int shippingId) {
--- src/test/java/fun/epoch/mall/service/ShippingServiceTest.java
+++ src/test/java/fun/epoch/mall/service/ShippingServiceTest.java
@@ -1,10 +1,19 @@
 package fun.epoch.mall.service;
 
 import fun.epoch.mall.dao.ShippingMapper;
+import fun.epoch.mall.entity.Shipping;
+import fun.epoch.mall.utils.response.ServerResponse;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import static fun.epoch.mall.common.enhanced.TestHelper.testIfCodeEqualsSuccess;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ShippingServiceTest {
@@ -13,4 +22,29 @@ public class ShippingServiceTest {
 
     @Mock
     ShippingMapper mapper;
+
+    /**
+     * 添加收货地址
+     * <p>
+     * 200  添加成功，返回新增收货地址的 id
+     */
+    @Test
+    public void testAddShipping_returnSuccess_withShippingId() {
+        when(mapper.insert(any())).thenAnswer((Answer<Integer>) invocation -> {
+            Shipping shipping = invocation.getArgument(0);
+            shipping.setId(shippingId);
+            return 1;
+        });
+
+        ServerResponse<Integer> response = testIfCodeEqualsSuccess(service.add(mock().build()));
+        assertEquals(shippingId, response.getData());
+    }
+
+    // 合法值
+    private static final Integer userId = 1000000;
+    private static final Integer shippingId = 1000000;
+
+    private Shipping.ShippingBuilder mock() {
+        return Shipping.builder().userId(userId).name("小明").mobile("15623336666").province("广东省").city("广州市").district("小谷围街道").address("宇宙工业大学");
+    }
 }
