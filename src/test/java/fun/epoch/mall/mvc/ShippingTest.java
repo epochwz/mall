e918c570305252f3416051033df80a48eb1e5535
epochwz <epoch.wz@gmail.com>
2019-10-20 13:29:51 +0800

    union-test: shipping -- setup

--- src/test/java/fun/epoch/mall/mvc/ShippingTest.java
+++ src/test/java/fun/epoch/mall/mvc/ShippingTest.java
@@ -1,6 +1,19 @@
 package fun.epoch.mall.mvc;
 
 import fun.epoch.mall.mvc.common.CustomMvcTest;
+import org.junit.Before;
+
+import static fun.epoch.mall.common.Constant.AccountRole.CONSUMER;
+import static fun.epoch.mall.mvc.common.Keys.MockSqls.COMMON_SQLS;
+import static fun.epoch.mall.mvc.common.Keys.Tables.shipping;
+import static fun.epoch.mall.mvc.common.Keys.UserKeys.userId;
 
 public class ShippingTest extends CustomMvcTest {
+    @Before
+    public void setup() {
+        this.init()
+                .session(userId, CONSUMER)
+                .database(COMMON_SQLS)
+                .launchTable(shipping);
+    }
 }
