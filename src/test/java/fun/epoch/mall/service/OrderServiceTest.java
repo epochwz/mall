05567c727456a530458ebff7b112ea67a98f5216
epochwz <epoch.wz@gmail.com>
2019-10-22 10:58:21 +0800

    unit-test: order -- service -- initial

---
+++ src/test/java/fun/epoch/mall/service/OrderServiceTest.java
@@ -0,0 +1,11 @@
+package fun.epoch.mall.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderServiceTest {
+    @InjectMocks
+    OrderService service;
+}
