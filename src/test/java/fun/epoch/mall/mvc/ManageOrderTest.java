25000d14c7ffd5aacb6a6f02254d178023d5b28f
epochwz <epoch.wz@gmail.com>
2019-10-25 13:47:01 +0800

    union-test: manage-order -- setup

--- src/test/java/fun/epoch/mall/mvc/ManageOrderTest.java
+++ src/test/java/fun/epoch/mall/mvc/ManageOrderTest.java
@@ -1,6 +1,19 @@
 package fun.epoch.mall.mvc;
 
 import fun.epoch.mall.mvc.common.CustomMvcTest;
+import org.junit.Before;
+
+import static fun.epoch.mall.common.Constant.AccountRole.MANAGER;
+import static fun.epoch.mall.mvc.common.Keys.MockSqls.COMMON_SQLS;
+import static fun.epoch.mall.mvc.common.Keys.Tables.*;
+import static fun.epoch.mall.mvc.common.Keys.UserKeys.userId;
 
 public class ManageOrderTest extends CustomMvcTest {
+    @Before
+    public void setup() {
+        this.init()
+                .session(userId, MANAGER)
+                .database(COMMON_SQLS)
+                .launchTable(order, order_item, shipping, product);
+    }
 }
